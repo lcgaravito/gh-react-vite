6e6507a024d5228358a6bdc958189ce43bc31285
lcgaravito <lc.garavito@uniandes.edu.co>
2023-05-24 04:00:02 -0500

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://luisgaravito.com/gh-react-vite",
+  base: "/gh-react-vite/",
   plugins: [react()],
 });
